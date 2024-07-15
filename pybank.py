9798f80ef270e5122742227adf166719a916ee62
Priti2901 <159483603+Priti2901@users.noreply.github.com>
2024-07-15 12:11:19 +0100

    Add files via upload

---
+++ pybank.py
@@ -0,0 +1,63 @@
+import os
+import csv
+
+py_bank=os.path.join('Resources','budget_data.csv')
+
+with open (py_bank) as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=',')
+
+    csv_header=next(csvreader)
+    
+    months=[]
+    profit_losses=[]
+    
+    total=0
+    a_change=0
+    m_change=0
+    m_count=0
+    delta1=0
+    delta2=0
+    delta_line1=0
+    delta_line2=0
+    loop1=0
+    loop2=0
+
+     
+    for row in csvreader:
+        month=row[0]
+        profit_loss=row[1]
+
+        months.append(month)
+        profit_losses.append(profit_loss)
+
+    m_count = len(months)
+
+for loop1 in range (m_count):
+    total = total + int(profit_losses[loop1])
+
+for loop2 in range(m_count-1):
+    a_change = a_change + (float(profit_losses[loop2+1]) - float(profit_losses[loop2]))
+
+    m_change = (float(profit_losses[loop2 +1 ])-float(profit_losses[loop2])) 
+
+    if m_change>delta1:
+        delta1 = m_change
+        delta_line1 = loop2
+    else:
+        delta2 = delta2
+
+analysis = f'\
+Financial Analysis\n\
+------------------------------------\n\
+Total Months: {m_count}\n\
+Total Amount: ${total}\n\
+Averag Change: ${round(a_change/(m_count-1),2)}\n\
+Greatest Increase in Profits: {months[delta_line1 + 1]} (${int(delta1)})\n\
+Greatest Decrease in profits: {months[delta_line2 + 1]} (${int(delta2)})\n'
+
+print(analysis)
+
+file1 = open("pybank.txt","w")
+file1.writelines(analysis)
+file1.close()
+
---
+++ pypoll.py
@@ -0,0 +1,86 @@
+import os
+import csv
+
+py_poll=os.path.join('Resources','election_data.csv')
+
+with open (py_poll) as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=',')
+
+    csv_header=next(csvreader)
+
+    ballot_ids=[]
+    counties=[]
+    candidates=[]
+    candidatenames=[]
+    total_vote_can=[]
+    result_per_can=[]
+    total_can_per=[]
+
+    line_count=0
+    winnervotes=0
+    loservote=0
+    loop1=0
+    loop2=0
+    loop3=0
+    loop4=0
+
+
+    for row in csvreader:
+        ballot_id=row[0]
+        county=row[1]
+        candidate=row[2]
+        ballot_ids.append(ballot_id)
+        counties.append(county)
+        candidates.append(candidate)
+    line_count=len(ballot_ids)
+
+candidatenames.append(candidates[0])
+
+for loop1 in range (line_count -1):
+    if candidates[loop1+1] !=candidates[loop1] and candidates[loop1+1] not in candidatenames:
+        candidatenames.append(candidates[loop1+1])
+
+n=len(candidatenames)
+
+for loop2 in range (n):
+    total_vote_can.append(candidates.count(candidatenames[loop2]))
+
+loservotes=line_count
+
+for loop3 in range(n):
+    total_can_per.append(f'{round(total_vote_can[loop3]/line_count*100),4}%')
+    if total_vote_can[loop3]>winnervotes:
+        winner=candidatenames[loop3]
+        winnervotes=total_vote_can[loop3]
+    if total_vote_can[loop3]<loservotes:
+        loser=candidatenames[loop3]
+        loservotes=total_vote_can[loop3]
+
+for loop4 in range(n):
+    result_per_can.append(f'{candidatenames[loop4]}: {total_can_per[loop4]} {total_can_per[loop4]}')
+
+resultlines='\n'.join(result_per_can)
+
+analysis=f'\
+Election Results\n\
+-------------------\n\
+Total Votes: {line_count}\n\
+-------------------\n\
+{resultlines}\n\
+-------------------\n\
+Winner: {winner} :)\n\
+Last: {loser} :(\n\
+-------------------\n'
+
+print(analysis)
+
+file1 = open("pypoll.txt","w")
+file1.writelines(analysis)
+file1.close()
+
+
+    
+
+
+    
+
